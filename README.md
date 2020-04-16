d3ced05b771e770c4eaff7cf5b5ab32a7860bf61
CristianX <thecristianx@hotmail.com>
2020-04-15 20:08:33 -0500

    Conectando servicios de lugar y clima en app.js

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 - `npm install axios`
 
 ## Url de Rapid API
-- https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=Lasso
+- Latitud y longitud https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=Lasso
+- Clima api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=sadsadsad&units=metric
--- app.js
+++ app.js
@@ -13,4 +13,26 @@ const argv = require('yargs').options({
 
 // Imprimiendo datos
 // lugar.getLugarLatLng(argv.direccion).then(console.log);
-clima.getClima(40.750000, -74.000000).then(console.log).catch(console.log);
+// Imprimiendo temperatura
+// clima.getClima(40.750000, -74.000000).then(console.log).catch(console.log);
+
+const getInfo = async(direccion) => {
+
+
+    try {
+
+        const coordenadas = await lugar.getLugarLatLng(direccion);
+        const temperatura = await clima.getClima(coordenadas.lat, coordenadas.lng);
+        return console.log(`El clima de ${ coordenadas.direccion } es de ${ temperatura }°C`);
+
+    } catch (error) {
+        return `No se puedo determinar el clima de ${ direccion }`;
+    }
+
+
+
+
+};
+
+
+getInfo(argv.direccion).then(console.log).catch(console.log);
